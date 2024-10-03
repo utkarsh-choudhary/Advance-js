282c42a2b6bfb7827a1ba4131c74ae5490a2a5ba
utkarsh-choudhary <90688693+utkarsh-choudhary@users.noreply.github.com>
2024-10-03 22:26:02 +0530

    datatype conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score=33;
+
+
+// console.log(score);
+// console.log(typeof score);
+
+
+let valueInNumber=Number(score)
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+
+// "33abc"=>NaN
+// true=>1;false=>0
+// ""=>0
+// undefined=>NaN
+
+
+
+let isLoggedIn=1;
+
+
+let login=Boolean(isLoggedIn);
+console.log(login);
+console.log(typeof login);
+
