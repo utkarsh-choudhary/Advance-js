6a65c0746ac20fefb8680869f6077efac668ca01
utkarsh-choudhary <90688693+utkarsh-choudhary@users.noreply.github.com>
2024-10-03 22:06:15 +0530

    basic datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"  //treat js code as newer version
+
+
+
+// primitive datatypes
+// number => 2 to the power 52
+// BigInt
+// string=> ""
+// Boolean => true/false
+// null
+// undefined
+// Symbol=>unique
+
+
+// non proimitve datatypes
+// Object
+
+
+console.log(typeof null);  //object
---
+++ 01_basics/text.js
@@ -0,0 +1,19 @@
+
+const accountId=144553;
+let accountEmail="utkarsh@google.com";
+var accountPassword="12345";
+accountCity="jaipur";
+let accountState;
+
+
+// accountId=12345 
+
+
+
+accountEmail="uc@uc.com";
+accountPassword="34743";
+accountCity="bengaluru";
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
